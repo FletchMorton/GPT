76323b68637c08b5a3841de2f98a30456fdff571
FletchMorton <morton.fletch@gmail.com>
2023-09-19 22:13:41 -0400

    input and output

--- Assignment1.py
+++ Assignment1.py
@@ -1,3 +1,7 @@
+#imports
+import sys
+import pathlib
+
 # Information about the process
 class Process:
     def __init__(self, pid, arrival_time, burst_time):
@@ -16,9 +20,9 @@ class Process:
 def fifo_scheduler(processes, runfor):
     current_time = 0
     process_queue = []
-    finished_processes = [] # Student Created
-    unfinished_processes = [] # Student Created
-    current_process = None  # Initialize to None
+    finished_processes = []     # (Student Created) Keep track of the processes which finished
+    unfinished_processes = []   # (Student Created) Keep track of the processes which had not finished yet
+    current_process = None      # Initialize to None
 
     while (current_time < runfor or process_queue or processes) and current_time <= runfor:
         # Add newly arrived processes to the queue and log the arrival time
@@ -27,7 +31,7 @@ def fifo_scheduler(processes, runfor):
             new_process.arrival_time = current_time
             process_queue.append(new_process)
             unfinished_processes.append(new_process) # Student Created
-            print(f"Time {current_time}: {new_process.pid} arrived") # Edited for formatting
+            outputFile.write(f"Time {current_time}: {new_process.pid} arrived\n") # Student edited for formatting
 
         if current_process is None and process_queue:
             current_process = process_queue.pop(0)
@@ -35,7 +39,7 @@ def fifo_scheduler(processes, runfor):
                 current_process.start_time = current_time
                 current_process.response_time = current_process.start_time - current_process.arrival_time
 
-            print(f"Time {current_time}: {current_process.pid} selected (burst {current_process.remaining_time})")
+            outputFile.write(f"Time {current_time}: {current_process.pid} selected (burst {current_process.remaining_time})\n")
 
         if current_process:
             if current_process.remaining_time > 0:
@@ -44,27 +48,34 @@ def fifo_scheduler(processes, runfor):
             else:
                 unfinished_processes.pop(0) # Student Created
                 current_process.finish_time = current_time
-                # Deleted wait_time, as it was unnecessary in this scheduler
+                # Student deleted "wait_time", as it was unnecessary in this scheduler
                 current_process.turnaround_time = current_process.finish_time - current_process.arrival_time
-                print(f"Time {current_time}: {current_process.pid} finished")
+                outputFile.write(f"Time {current_time}: {current_process.pid} finished\n")
                 finished_processes.append(current_process) # Student Created
                 current_process = None  # Set to None to select the next process
 
         else:
-            print(f"Time {current_time}: Idle")
+            outputFile.write(f"Time {current_time}: Idle\n")
             current_time += 1  # Idle CPU time when no process is in the queue
 
-    # Student Intervention: I reworked the method by which processes were considered finished/unfinished. As such, code was removed here
+    # Student Intervention: I reworked the method by which processes were considered finished/unfinished. 
+    # As such, code was removed here, and the code below was added. This is a complete rework
 
-    print(f"Finished at time {runfor}\n")
+    outputFile.write(f"Finished at time {runfor}\n")
+    
+    if unfinished_processes:
+        unfinished_processes.sort(key=lambda process: process.pid)
+
+    if finished_processes:
+        finished_processes.sort(key=lambda process: process.pid)
 
     for unfin in unfinished_processes:
-        print(f"{unfin.pid} did not finish")
+        outputFile.write(f"{unfin.pid} did not finish\n")
 
-    print("\n")
+    outputFile.write("\n")
 
     for process in finished_processes:
-        print(f"{process.pid}\twait\t{process.wait_time}\tturnaround\t{process.turnaround_time}\tresponse\t{process.response_time}")
+        outputFile.write(f"{process.pid}\twait\t{process.wait_time}\tturnaround\t{process.turnaround_time}\tresponse\t{process.response_time}\n")
 
 
 #SJF
@@ -82,7 +93,7 @@ def preemptive_sjf_scheduler(processes, runfor):
             new_process.arrival_time = current_time
             process_queue.append(new_process)
             unfinished_processes.append(new_process) # Student Created
-            print(f"Time {current_time}: {new_process.pid} arrived") # Edited for formatting
+            outputFile.write(f"Time {current_time}: {new_process.pid} arrived\n") # Student edited for formatting
 
         # Sort the process queue based on remaining time
         process_queue.sort(key=lambda x: x.remaining_time)
@@ -95,16 +106,16 @@ def preemptive_sjf_scheduler(processes, runfor):
                     current_process.start_time = current_time
                     current_process.response_time = current_process.start_time - current_process.arrival_time
 
-                print(f"Time {current_time}: {current_process.pid} selected (burst {current_process.remaining_time})")
+                outputFile.write(f"Time {current_time}: {current_process.pid} selected (burst {current_process.remaining_time})\n")
 
             elif process_queue[0].remaining_time < current_process.remaining_time:
                 # Preempt the current process if a shorter one arrives
-                print(f"Time {current_time}: {current_process.pid} preempted by {process_queue[0].pid}")
+                outputFile.write(f"Time {current_time}: {current_process.pid} preempted by {process_queue[0].pid}\n")
                 process_queue.append(current_process)
                 current_process = process_queue[0]
                 if current_process.start_time is None:
                     current_process.start_time = current_time
-                print(f"Time {current_time}: {current_process.pid} selected (burst {current_process.remaining_time})")
+                outputFile.write(f"Time {current_time}: {current_process.pid} selected (burst {current_process.remaining_time})\n")
 
             if current_process.remaining_time > 0:
                 current_process.remaining_time -= 1
@@ -115,26 +126,33 @@ def preemptive_sjf_scheduler(processes, runfor):
                     current_process.finish_time = current_time
                     current_process.wait_time = current_process.start_time - current_process.arrival_time
                     current_process.turnaround_time = current_process.finish_time - current_process.arrival_time
-                    print(f"Time {current_time}: {current_process.pid} finished")
+                    outputFile.write(f"Time {current_time}: {current_process.pid} finished\n")
                     finished_processes.append(current_process) # Student Created
                     process_queue.pop(0)  # Remove completed process from the queue
                     current_process = None
 
         else:
-            print(f"Time {current_time}: Idle")
+            outputFile.write(f"Time {current_time}: Idle\n")
             current_time += 1
 
-    # Student Intervention: I reworked the method by which processes were considered finished/unfinished. As such, code was removed here
+    # Student Intervention: I reworked the method by which processes were considered finished/unfinished. 
+    # As such, code was removed here, and the code below was added. This is a complete rework
 
-    print(f"Finished at time {runfor}\n")
+    outputFile.write(f"Finished at time {runfor}\n")
+
+    if unfinished_processes:
+        unfinished_processes.sort(key=lambda process: process.pid)
+
+    if finished_processes:
+        finished_processes.sort(key=lambda process: process.pid)
 
     for unfin in unfinished_processes:
-        print(f"{unfin.pid} did not finish")
+        outputFile.write(f"{unfin.pid} did not finish\n")
 
-    print("\n")
+    outputFile.write("\n")
 
     for process in finished_processes:
-        print(f"{process.pid}\twait\t{process.wait_time}\tturnaround\t{process.turnaround_time}\tresponse\t{process.response_time}")
+        outputFile.write(f"{process.pid}\twait\t{process.wait_time}\tturnaround\t{process.turnaround_time}\tresponse\t{process.response_time}\n")
 
 
 #RR
@@ -152,7 +170,7 @@ def round_robin_scheduler(processes, time_quantum):
             new_process.arrival_time = current_time
             process_queue.append(new_process)
             unfinished_processes.append(new_process) # Student Created
-            print(f"Time {current_time}: {new_process.pid} arrived") #Edited for formatting
+            outputFile.write(f"Time {current_time}: {new_process.pid} arrived\n") #Student edited for formatting
 
         if process_queue:
             current_process = process_queue.pop(0)
@@ -161,7 +179,7 @@ def round_robin_scheduler(processes, time_quantum):
                 current_process.response_time = current_process.start_time - current_process.arrival_time
                 current_process.selected_time = current_time
 
-            print(f"Time {current_time}: {current_process.pid} selected (burst {current_process.remaining_time})")
+            outputFile.write(f"Time {current_time}: {current_process.pid} selected (burst {current_process.remaining_time})\n")
 
             if current_process.remaining_time <= time_quantum:
                 current_time += current_process.remaining_time
@@ -171,14 +189,14 @@ def round_robin_scheduler(processes, time_quantum):
                     if process is not current_process and process.selected_time is not None:
                         if process.selected_time < current_time:
                             process.wait_time += current_process.remaining_time
-                            print(f"{process.pid} is waiting for {current_process.remaining_time}")
+                            outputFile.write(f"{process.pid} is waiting for {current_process.remaining_time}\n")
 
                 unfinished_processes.pop(0) # Student Created
                 current_process.finish_time = current_time
                 current_process.turnaround_time = current_process.finish_time - current_process.arrival_time
                 current_process.remaining_time = 0
                 finished_processes.append(current_process) # Student Created
-                print(f"Time {current_time}: {current_process.pid} finished")
+                outputFile.write(f"Time {current_time}: {current_process.pid} finished\n")
 
             else:
                 current_time += time_quantum
@@ -188,31 +206,39 @@ def round_robin_scheduler(processes, time_quantum):
                     if process is not current_process and process.selected_time is not None:
                         if process.selected_time < current_time:
                             process.wait_time += time_quantum
-                            print(f"{process.pid} is waiting for {time_quantum}")
+                            outputFile.write(f"{process.pid} is waiting for {time_quantum}\n")
+                # ---------------------------------------------------
 
                 current_process.remaining_time -= time_quantum
                 process_queue.append(current_process)
-                print(f"Time {current_time}: {current_process.pid} (remaining burst {current_process.remaining_time})")
+                outputFile.write(f"Time {current_time}: {current_process.pid} (remaining burst\t{current_process.remaining_time})\n")
 
         elif processes:
             # No processes in the queue, but some are waiting to arrive
             current_time += 1
-            print(f"Time {current_time}: Idle")
+            outputFile.write(f"Time {current_time}: Idle\n")
 
         else:
             # No processes in the queue or waiting to arrive
-            print(f"Finished at time {current_time}\n")
+            outputFile.write(f"Finished at time\t{current_time}\n")
             break
 
-    # Student Intervention: I reworked the method by which processes were considered finished/unfinished. As such, code was removed here
+    # Student Intervention: I reworked the method by which processes were considered finished/unfinished. 
+    # As such, code was removed here, and the code below was added. This is a complete rework
+
+    if unfinished_processes:
+        unfinished_processes.sort(key=lambda process: process.pid)
+
+    if finished_processes:
+        finished_processes.sort(key=lambda process: process.pid)
 
     for unfin in unfinished_processes:
-        print(f"{unfin.pid} did not finish")
+        outputFile.write(f"{unfin.pid} did not finish\n")
 
-    print("\n")
+    outputFile.write("\n")
 
     for process in finished_processes:
-        print(f"{process.pid}\twait\t{process.wait_time}\tturnaround\t{process.turnaround_time}\tresponse\t{process.response_time}")
+        outputFile.write(f"{process.pid}\twait\t{process.wait_time}\tturnaround\t{process.turnaround_time}\tresponse\t{process.response_time}\n")
 
 
 # Main
@@ -235,12 +261,18 @@ if __name__ == "__main__":
     use = None
     quantum = 0
     end = False
-    file_path = "inp.txt"
+    file_path = sys.argv[1]
+
+    # (Student Created) Create an output file
+    output = pathlib.Path(file_path).stem
+    output += '.out'
+    
+    outputFile = open(output, 'w')
 
     # Read the file and collection information
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r') as inputFile:
         # For each line within the file
-        for line in file:
+        for line in inputFile:
             # Ignore comments starting with '#' and strip leading/trailing whitespace
             line = line.split('#')[0].strip()
             
@@ -269,7 +301,9 @@ if __name__ == "__main__":
 
             # This must be some type of error
             else:
-                print("There was an issue with the file formatting.")
+                outputFile.write("There was an issue with the file formatting.\n")
+
+    inputFile.close()
 
 
     # Add the collected information to their respective values
@@ -277,7 +311,7 @@ if __name__ == "__main__":
         match directive:
             case "processcount":
                 processcount = directives[directive]
-                print(f"{processcount} processes")
+                outputFile.write(f"{processcount} processes\n")
             case "runfor":
                 runfor = int(directives[directive])
             case "quantum":
@@ -290,11 +324,13 @@ if __name__ == "__main__":
     # Execute the requested scheduling algorithm
     match use:
         case "fifo":
-            print("Using First In First Out")
+            outputFile.write("Using First In First Out\n")
             fifo_scheduler(processes, runfor)
         case "sjf":
-            print("Using preemptive Shortest Job First")
+            outputFile.write("Using preemptive Shortest Job First\n")
             preemptive_sjf_scheduler(processes, runfor)
         case "rr":
-            print("Using preemptive Round Robin")
+            outputFile.write("Using preemptive Round Robin\n")
             round_robin_scheduler(processes, quantum)
+
+    outputFile.close()
